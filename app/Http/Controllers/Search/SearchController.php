25356cad7313a6b3a43007b9d904c24e5419659c
nofloat <304546210@qq.com>
2016-11-08 20:25:29 +0800

    add

--- app/Http/Controllers/Search/SearchController.php
+++ app/Http/Controllers/Search/SearchController.php
@@ -16,32 +16,35 @@ class SearchController extends Controller
      */
     public function index()
     {
+        $this->getNewMovies();
+
+        EXIT;
         $client = new Client();
-        $request = $client->request('GET','http://172.22.161.66:9200/jwzx/category/_search',
+        $request = $client->request('POST','http://172.22.161.66:9200/jwzx/category/_search',
             [
-                // 'query' => 'q=text:教务',
-                // 'highlight'=>'fields=about:教务'
-                // 'json'=>[
-                //     'query' =>[
-                //         'term'=>[
-                //             'text' => '教务'
-                //         ]
-                //     ]
-                // ]
                 'json'=>[
                     'query' =>[
                         'match'=>[
                             'text' => '教务'
                         ]
                     ],
-                    "highlight"=>[
-                        "fields" => [
-                             "text" => []
+                    'highlight'=>[
+                        'pre_tags' => '<div color ="red">',
+                        'post_tags'=> '</div>',
+                        'fields' => [
+                            'text' => (object)[]
                         ]
                     ]
                 ]
             ]);
-        echo $request->getBody();exit;
+
+        $goal = json_decode($request->getBody());
+        $goal_array = $goal->hits->hits;
+        
+        foreach ($goal_array as $key => $value) {
+            echo $goal_array[$key]->highlight->text[0];exit;
+        }
+        exit;
         $a = new \Elasticsearch\ClientBuilder();
         $search =  $a->create()->setHosts(['172.22.161.66:9200'])->build();
         $params = [
@@ -55,8 +58,9 @@ class SearchController extends Controller
   
                 ],
                 'highlight'=> [
-                    "pre_tags"  => '<em>',
-                    "post_tags" => '</em>',
+                    'pre_tags'  => '<em>',
+                    'post_tags' => '</em>',
+                    'text' =>[]
                 ]
             ]
         ];
@@ -64,6 +68,32 @@ class SearchController extends Controller
         print_r($response);
 
         //var_dump($search);
+    }
+
+    public function getNewMovies(){
+        $client = new Client();
+        $request = $client->request('POST','http://172.22.161.11/index/',
+            [   
+                'query' => [
+                    'id' => '1'
+                ]
+            ]);
+
+        $content=$request->GetBody();
+        echo $content;exit;
+        $pattern = '<div class="poster">
+        <a href="javascript:void(0)" url="/torrent/showInfo/id/7452.html" class="window">
+            <img src="http://172.22.161.11/upload/pic/2c38b094a17e875661b36a1edc5811aa.jpg" alt="【美国】自杀小队"  />
+        </a>
+    </div>';
+        $goal = $this->_patternGoal($pattern,$content);
+
+        exit; 
+    }
+
+    private function _patternGoal($pattern,$string){//匹配函数
+        preg_match_all($pattern,$string,$goalarray);
+        return $goalarray;
     }
 
     /**
